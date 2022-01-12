f6c3d46fca62846a89d3ff07bf6940baa4a9d46a
Bryant-Barron <bryant.barron@gmail.com>
2022-01-12 14:20:22 -0800

    added more practice

--- day4/day4.py
+++ day4/day4.py
@@ -1,5 +1,29 @@
 # Day 4 Randomisation and Python Lists
 import random
+import my_module
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# print(my_module.pi)
+
+# random_float = random.random()
+# #generates a random number from 0 - 9.99999 so just doesnt include 10
+# random_float = random_float * 10
+# print(random_float)
+
+# love_score = random.randint(1,100)
+# print(f"Your love score is: {love_score}")
+
+#code challenge heads or tails with
+# coin = random.randint(0,1)
+# if coin == 0:
+#     print("Tails")
+# elif coin == 1:
+#     print("Heads")
+# else:
+#     print("Broken")
+
+state1 = "Delaware"
+state2 = "Pennsylvania"
+
---
+++ practice/printfxn.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+i = 1 
+num = ""
+while i <= n:
+    num = num + str(i)
+    i += 1
+
+print(num)
