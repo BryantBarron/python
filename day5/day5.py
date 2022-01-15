9a733424637e19b8bfbe470d887bae3017afce12
Bryant-Barron <bryant.barron@gmail.com>
2022-01-15 01:52:13 -0800

    day5 continued

--- day5/day5.py
+++ day5/day5.py
@@ -5,16 +5,34 @@
 # print(fruits)
 
 #average student height
-student_heights = input("Input a list of student heights ").split()
-total_heights = 0
-count = 0
-for n in range(0, len(student_heights)):
-    student_heights[n] = int(student_heights[n])
-    total_heights = total_heights + int(student_heights[n])
-    count = count + 1
+# student_heights = input("Input a list of student heights ").split()
+# total_heights = 0
+# count = 0
+# for n in range(0, len(student_heights)):
+#     student_heights[n] = int(student_heights[n])
+#     total_heights = total_heights + int(student_heights[n])
+#     count = count + 1
 
-average_height = total_heights / count
-average_height = round(average_height, 2)
+# average_height = total_heights / count
+# average_height = round(average_height, 2)
 
-print(f"The Average Height is: {average_height}")
+# print(f"The Average Height is: {average_height}")
+
+#student scores
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+highest_score = 0
+lowest_score = 100
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+    if score < lowest_score:
+        lowest_score = score
+        
+print(f"The highest score is: {highest_score}")
+print(f"The lowest score is: {lowest_score}")
+
 
