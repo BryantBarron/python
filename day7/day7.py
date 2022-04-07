edbbb101d693191f7afc482d5e116c569084c965
Kareless <bryant.barron@gmail.com>
2022-04-06 19:55:29 -0700

    continued

--- day7/day7.py
+++ day7/day7.py
@@ -17,12 +17,13 @@ for char in game_word:
     word += char
     filled_word += "_"
 
-print(game_word)
-print(word)
 print(filled_word)
     
+print("\n")
 
-while(len(incorrect_guesses) <= 5):
+end_of_game = False
+while not end_of_game:
+    guess_count = 0
     guess = input("Guess a letter: ") 
 
     while((guess.isalpha() != True)):
@@ -30,7 +31,6 @@ while(len(incorrect_guesses) <= 5):
         guess = input("Guess a letter: ")
 
     guess = guess.lower()
-
     if guess in game_word:
         print("The letter you guessed is in the word!")
         correct_guesses += guess
@@ -40,10 +40,16 @@ while(len(incorrect_guesses) <= 5):
                 filled_word[position] = i
                 
         print(filled_word)
+    elif guess in incorrect_guesses:
+        print(f"You already guessed: {guess}")
     else:
         print("The letter you guessed is NOT in the word!")
         incorrect_guesses += guess
+        print(f"Here are your guesses so far: {incorrect_guesses}")
+        if(len(incorrect_guesses) == 5):
+            print("You have killed your hangman!")
+            end_of_game = True
         
         
-
+        
 
