c54dd382e7745fe29478e9b20eb3fa751e758b9f
Kareless <bryant.barron@gmail.com>
2022-04-12 01:01:46 -0700

    dictionary lesson

---
+++ day9/dictionaries.py
@@ -0,0 +1,25 @@
+programming_dictionary = {
+    "Bug" : "An error in the program that prevents the program from running as expected.",
+    "Function" : "A piece of code that you can easily call over and over again", 
+    "Loop" : "The action of doing something over and over again",
+}
+
+print(programming_dictionary["Bug"])
+
+#creating an empty dictionary
+empty_dictionary = {}
+
+#wipe an existing dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+#edit an item in a dicitonary
+programming_dictionary["Bug"] = "Test"
+print(programming_dictionary)
+
+#loop through a dictionary
+for thing in programming_dictionary:
+    #only prints the key, not the value in the key
+    print(thing)
+    #to get value in key
+    print(programming_dictionary[thing])
